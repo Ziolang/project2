7bb7528dcd994cdd613a3afcd78ba203460739cd
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-19 08:42:50 -0700

    welp

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/getPerson/:id', getPerson)
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 const connectionString = process.env.DATABASE_URL || "postgres://lsetltiaqifutt:a9820380e07bcefd62806e966634d27a4d57723db2509436a8f7bcab71279927@ec2-174-129-253-174.compute-1.amazonaws.com:5432/denvgq60nkajt6?ssl=true";
 const pool = new Pool(
 	{
@@ -14,23 +22,16 @@ const pool = new Pool(
 
 var sql = "SELECT * FROM Person";
 pool.query(sql, function(err, result) {
-			// If an error occurred...
-			if (err) {
-						console.log("Error in query: ")
-						console.log(err);
-					}
+	// If an error occurred...
+	if (err) {
+		console.log("Error in query: ")
+		console.log(err);
+	}
 
-			// Log this to the console for debugging purposes.
-			console.log("Back from DB with result:");
-			console.log(result.rows);
-		}
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/getPerson/:id', getPerson)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	// Log this to the console for debugging purposes.
+	console.log("Back from DB with result:");
+	console.log(result.rows);
+}
 
 function getPerson(req, res){ 
 	var person = req.params;
