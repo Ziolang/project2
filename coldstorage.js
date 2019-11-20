4ebb3171269f9054785146fc20b8203f76c505da
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-19 22:31:18 -0700

    It starts

---
+++ coldstorage.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/getPerson', getPerson)
+  .get('/getParents/:id', getParents)
+  .get('/getChild/:id', getChild)
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+const connectionString = process.env.DATABASE_URL || "postgres://lsetltiaqifutt:a9820380e07bcefd62806e966634d27a4d57723db2509436a8f7bcab71279927@ec2-174-129-253-174.compute-1.amazonaws.com:5432/denvgq60nkajt6?ssl=true";
+const pool = new Pool(
+	{
+	//connectionString: process.env.DATABASE_URL,
+	//ssl:true
+		connectionString: connectionString
+	});
+
+var sql = "SELECT * FROM Person";
+pool.query(sql, function(err, result) {
+	// If an error occurred...
+	if (err) {
+		console.log("Error in query: ")
+		console.log(err);
+	}
+
+	// Log this to the console for debugging purposes.
+	console.log("Back from DB with result:");
+	console.log(result.rows);
+});
+
+function getPerson(req, res){ 
+	var sql = "SELECT * FROM Person";
+
+	pool.query(sql, function(err, result) {
+	// If an error occurred...
+	if (err) {
+		console.log("Error in query: ")
+		console.log(err);
+	}
+
+	// Log this to the console for debugging purposes.
+	console.log("Back from DB with result:");
+	res.send(result.rows);
+});
+}
+
+
+
+/* NONE OF THIS WORKS FULLY
+function getParents(req, res){ 
+	var relationship = "SELECT * FROM Relationship WHERE child = " + req.params['id'];
+	var father = "";
+	var mother = "";
+
+	pool.query(relationship, function(err, result) {
+	// If an error occurred...
+		if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+		}
+
+		father = "SELECT * FROM Person WHERE Person.id = " + result['father'];
+		mother = "SELECT * FROM Person WHERE Person.id = " + result['mother'];
+
+		pool.query(father, function(err, pop) {
+		// If an error occurred...
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+			}
+			console.log(pop);
+			res.send(pop);
+		});
+		pool.query(mother, function(err, mom) {
+		// If an error occurred...
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+			}
+			console.log(mom);
+			res.send(mom);
+		});
+	});
+}
+
+function getChild(req, res){ 
+	var sql = "SELECT * FROM Person";
+
+	pool.query(sql, function(err, result) {
+	// If an error occurred...
+	if (err) {
+		console.log("Error in query: ")
+		console.log(err);
+	}
+
+	// Log this to the console for debugging purposes.
+	console.log("Back from DB with result:");
+	res.send(result.rows);
+});
+}*/
--- database.sql
+++ database.sql
@@ -2,16 +2,12 @@ CREATE TABLE Cells (
 	id SERIAL NOT NULL PRIMARY KEY, 
 	name varchar(100) NOT NULL, 
 	content char NOT NULL, 
-	hexColor varchar NOT NULL, 
+	hexColor varchar, 
 	blocks boolean NOT NULL,
 	durability boolean,
-	dr int,
+	pr int,
 	mr int, 
 	detail varchar(1000)
-);
-
-CREATE TABLE stats (
-	id SERIAL NOT NULL PRIMARY KEY,
 );
 
 CREATE TABLE Positions ( 
@@ -21,15 +17,21 @@ CREATE TABLE Positions (
 	grid_name varchar(100) NOT NULL
 );
 
-CREATE TABLE Grids (
-	id SERIAL NOT NULL PRIMARY KEY, 
-	name varchar(100) NOT NULL,
-	author varchar(100)
-);
-
 INSERT INTO Cells
 VALUES 
 	(DEFAULT, 'Tree', 'T', '009617', true, true, 3, 1, 'A vertical log with leaves!'),
 	(DEFAULT, 'Water', '~', '001eff', true, false, NULL, NULL, 'A calm bit of H2O.'),
 	(DEFAULT, 'Wall', 'W', '8f8f8f', true, true, 4, 4, 'A solid obsticle. It could be damaged, but it looks pretty sturdy.'),
-	(DEFAULT, 'Door', 'D', '8f8f8f', true, true, 2, 2, 'It can be opened!'),
+	(DEFAULT, 'Door', 'D', '8f8f8f', true, true, 2, 2, 'It can be opened.');
+
+INSERT INTO Positions
+VALUES 
+	(DEFAULT, 'A1', 1, 'Sample'),
+	(DEFAULT, 'B2', 1, 'Sample'),
+	(DEFAULT, 'C3', 1, 'Sample'),
+	(DEFAULT, 'D4', 1, 'Sample'),
+	(DEFAULT, 'E5', 1, 'Sample'),
+	(DEFAULT, 'F6', 1, 'Sample'),
+	(DEFAULT, 'G7', 1, 'Sample'),
+	(DEFAULT, 'H8', 1, 'Sample'),
+	(DEFAULT, 'I9', 1, 'Sample');
