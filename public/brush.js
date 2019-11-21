167ef80b39a0ee2d96c27768b930870ce1d59f47
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-21 15:50:46 -0700

    It starts

--- public/brush.js
+++ public/brush.js
@@ -1,12 +1,13 @@
-/*function selectBrush(select) {
-	var count = document.getElementById('count');
-	var reset = ".";
-	for (var i = 1; i <= count; i++) {
-		reset += i;
-		$(reset).css("visibility", "hidden");
-		reset = ".";
-	}
 
-	var brush = "." + select.value;
-	$(brush).css("visibility", "visible");
-}*/
+		function selectBrush(select) {
+			var count = document.getElementById('count');
+			var reset = ".";
+			for (var i = 1; i <= count; i++) {
+				reset += i;
+				document.getElementById(reset).style.visibility = "hidden";
+				reset = ".";
+			}
+
+			var brush = "." + select.value;
+			document.getElementById(brush).style.visibility = "hidden";
+		}
