848f313a785f184e263fe0269797ad615d26a2bd
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-23 11:22:40 -0700

    It starts

--- models/cellModel.js
+++ models/cellModel.js
@@ -1,9 +1,9 @@
 function getCellById(id, callback) {
-	const sql = const sql = "SELECT * FROM Cells WHERE id = " + id;
+	/*const sql = const sql = "SELECT * FROM Cells WHERE id = " + id;
 	pool.query(sql, function(err, result) {
 		if (err) console.log(err);
 		callback(null, result.row[0]);
-	});
+	});*/
 }
 
 module.exports = {
