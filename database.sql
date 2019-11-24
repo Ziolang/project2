e6ac93997a367d1e61fe30ca1f34633083a7eda0
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-24 10:26:53 -0700

    It starts

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE TABLE Cells (
 	id SERIAL NOT NULL PRIMARY KEY, 
 	name varchar(100) NOT NULL, 
 	content char NOT NULL, 
-	color varchar(6), 
+	color varchar(7), 
 	blocks boolean NOT NULL,
 	durability int,
 	pr varchar(20),
@@ -11,10 +11,10 @@ CREATE TABLE Cells (
 );
 INSERT INTO Cells
 VALUES 
-	(DEFAULT, 'Tree', 'T', '009617', true, 100, 'Moderate.', 'Very Low.', 'A vertical log with leaves!'),
-	(DEFAULT, 'Water', '~', '001eff', true, NULL, NULL, NULL, 'A calm bit of H2O.'),
-	(DEFAULT, 'Wall', 'W', '8f8f8f', true, 100, 'High.', 'High', 'A solid obsticle. It could be damaged, but it looks pretty sturdy.'),
-	(DEFAULT, 'Door', 'D', '8f8f8f', true, 100, 'Low.', 'Low.', 'It can be opened.');
+	(DEFAULT, 'Tree', 'T', '#009617', true, 100, 'Moderate.', 'Very Low.', 'A vertical log with leaves!'),
+	(DEFAULT, 'Water', '~', '#001eff', true, NULL, NULL, NULL, 'A calm bit of H2O.'),
+	(DEFAULT, 'Wall', 'W', '#8f8f8f', true, 100, 'High.', 'High', 'A solid obsticle. It could be damaged, but it looks pretty sturdy.'),
+	(DEFAULT, 'Door', 'D', '#8f8f8f', true, 100, 'Low.', 'Low.', 'It can be opened.');
 
 
 CREATE TABLE Positions ( 
