2fd29ef21bb9189cb69e878a49a2482135103158
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-23 23:03:33 -0700

    It starts

--- public/checkdb.js
+++ public/checkdb.js
@@ -5,7 +5,7 @@ function searchById() {
 	console.log("Cell:" + cell);
 
 	$.get("/getCell", {cellID:cell}, function(data) {
-		var item = data;
+		var item = data.list.name;
 		console.log("Got: " + item);
 		$("#result").html(item);
 	})
