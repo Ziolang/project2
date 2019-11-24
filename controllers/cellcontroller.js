f5a7a04d27242a4c4511e5726d5c478ec4313a7c
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-23 22:31:29 -0700

    It starts

--- controllers/cellcontroller.js
+++ controllers/cellcontroller.js
@@ -1,7 +1,7 @@
 const cellModel = require('../models/cellModel.js')
 
 function getCell(req, res) {
-	var cellID = req.query.id;
+	var cellID = req.query.cellID;
 	cellModel.getCellById(cellID, function(err, result) {
 		res.json(result);
 	});
