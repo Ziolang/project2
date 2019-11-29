3cdaca6d04a14629ae04a744e4bdf8ed6a625ff8
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-11-29 12:35:06 -0700

    fixing post

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const cellController = require('./controllers/cellcontroller.js');
 
 express()
 	.use(express.static(path.join(__dirname, 'public')))
-	.use( bodyParser.json()
+	.use( bodyParser.json())
 	.use(bodyParser.urlencoded({extended: true}))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
 	.get('/getCell', cellController.getCell)
 	.get('/getCells', cellController.getCells)
 	.post('/postCell', cellController.postCell)
-
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 const connectionString = process.env.DATABASE_URL || "postgres://lsetltiaqifutt:a9820380e07bcefd62806e966634d27a4d57723db2509436a8f7bcab71279927@ec2-174-129-253-174.compute-1.amazonaws.com:5432/denvgq60nkajt6?ssl=true";
 const pool = new Pool(
