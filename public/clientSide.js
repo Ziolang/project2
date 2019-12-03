93bddbad3cc282cc4df4283fbd24531a7662c91d
stumpyfletcher@gmail.com <stumpyfletcher@gmail.com>
2019-12-03 10:54:00 -0700

    fixing post

--- public/clientSide.js
+++ public/clientSide.js
@@ -61,7 +61,7 @@ function initImgGrid() {
 		for (var c = 0; c < columns; c++) {
 			letter = String.fromCharCode(letterx);
 			position = letter + (c + 1);
-			grid += '<img src="fourway.png" class="cell" id="' + position + '" onclick="applyImgCell(this)"> ';
+			grid += '<img src="fourway.png" class="imgCell" id="' + position + '" onclick="applyImgCell(this)">';
 		}
 		grid += " " + letter + "<br/>";
 		letterx++;
